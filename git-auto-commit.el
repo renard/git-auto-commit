4aba4a30883744e759028f4af450f60ff2d54f0c
Łukasz Gruner <lukasz@gruner.lu>
2015-01-16 07:14:38 +0100

    Skip ignored files, ask about untracked files
    Before this change git add failed on ignored files.
    
    Also added option to define behaviour when committing untracked
    files.

--- git-auto-commit.el
+++ git-auto-commit.el
@@ -90,39 +90,46 @@ passed to `format' with the saved filename in parameter."
   :type 'string
   :group 'git-auto-commit)
 
+(defcustom gac-on-untracked-file 'ask
+  "How to act when current file is untracked."
+  :type '(choice (const :tag "Ask if file should be added" ask)
+                 (const :tag "Add without asking" t)
+                 (const :tag "Skip file" nil))
+  :group 'git-auto-commit)
+
 (defun gac-get-repositories ()
   "Extract directory list from `gac-dir-set'."
   (loop for x in gac-dir-set
-	collect (car x)))
+        collect (car x)))
 
 (defun gac-get-repo-config (repo)
   "Extract repository configuration from `gac-dir-set'."
   (let* ((repository (assoc repo gac-dir-set))
-	 (conf (copy-alist (cdr repository))))
+         (conf (copy-alist (cdr repository))))
     (when repository
       ;; put default values to repository if not defined.
       (loop for x in '(schedule-push-delay cmd-git-add cmd-git-commit cmd-git-push)
-	    do (setq conf
-		     (plist-put conf (intern (format ":%s" x))
-				(or (plist-get conf (intern (format ":%s" x)))
-				    (eval (intern (format "gac-default-%s" x)))))))
+            do (setq conf
+                     (plist-put conf (intern (format ":%s" x))
+                                (or (plist-get conf (intern (format ":%s" x)))
+                                    (eval (intern (format "gac-default-%s" x)))))))
       conf)))
 
 (defun gac-match-filep (f)
   "Test if file F is in a subdirectory of `gac-dir-set'."
   (car
    (remove 'nil
-	   (mapcar
-	    (lambda(x)
-	      (setq x (abbreviate-file-name
-		       (file-name-as-directory x)))
-	      (when
-		  (and
-		   (file-directory-p (concat x ".git"))
-		   (ignore-errors
-		     (string-match x (substring f 0 (length x)))))
-		x))
-	    (gac-get-repositories)))))
+           (mapcar
+            (lambda(x)
+              (setq x (abbreviate-file-name
+                       (file-name-as-directory x)))
+              (when
+                  (and
+                   (file-directory-p (concat x ".git"))
+                   (ignore-errors
+                     (string-match x (substring f 0 (length x)))))
+                x))
+            (gac-get-repositories)))))
 
 (defun gac-schedule-push (dn conf)
   "Schedule a push if one is not already scheduled for the given dir."
@@ -132,24 +139,39 @@ passed to `format' with the saved filename in parameter."
      (plist-get conf :schedule-push-delay) nil
      (lambda (dn conf)
        (let* ((default-directory (file-name-as-directory dn)))
-	 (message "Pushing git repository from  %s" dn)
-	 (shell-command (plist-get conf :cmd-git-push))))
+         (message "Pushing git repository from  %s" dn)
+         (shell-command (plist-get conf :cmd-git-push))))
      dn conf)))
+
+(defun gac-get-file-ignored-or-untracked (git-root fn)
+  "Check if file FN is ignored or untracked and return matching symbol or nil otherwise."
+  (let ((default-directory git-root))
+    (cond
+     ((not (string-blank-p (shell-command-to-string (format "git ls-files -i -o --exclude-standard %s" fn)))) :ignored)
+     ((not (vc-git-registered fn)) :untracked)
+     (t nil))))
 
 ;;;###autoload
 (defun gac-commit-file ()
   "Commit file visited in current buffer."
   (interactive)
   (let* ((fn (abbreviate-file-name (buffer-file-name)))
-	 (dn (gac-match-filep fn)))
-    (when dn
-      (message "git adding %s" fn)
-      (let* ((rn (file-relative-name fn dn))
-	     (default-directory dn)
-	     (dirname (directory-file-name dn))
-	     (conf (gac-get-repo-config dirname)))
-	(shell-command (format (plist-get conf :cmd-git-add) rn))
-	(shell-command (format (plist-get conf :cmd-git-commit) rn))
-	(gac-schedule-push dirname conf)))))
+         (dn (gac-match-filep fn))
+         (rn (file-relative-name fn dn))
+         (status (gac-get-file-ignored-or-untracked dn rn)))
+    (when (and dn
+               (not (eq status :ignored))
+               (if (eq status :untracked)
+                   (cond
+                    ((eq gac-on-untracked-file 'ask) (y-or-n-p (format "Add file \"%s\" to the index?" rn)))
+                    (t gac-on-untracked-file))
+                 t))
+      (message "git adding %s" rn)
+      (let* ((default-directory dn)
+             (dirname (directory-file-name dn))
+             (conf (gac-get-repo-config dirname)))
+        (shell-command (format (plist-get conf :cmd-git-add) rn))
+        (shell-command (format (plist-get conf :cmd-git-commit) rn))
+        (gac-schedule-push dirname conf)))))
 
 (provide 'git-auto-commit)
